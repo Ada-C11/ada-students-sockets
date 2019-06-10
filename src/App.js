59fe9774dcee87d97025d7dd5686521e3bc5b44e
Devin Helmgren <devincollege@gmail.com>
2019-06-10 11:20:25 -0700

    added first component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Student from './components/Student';
 
 
 const goofy_func = (a, b) => {
@@ -13,20 +14,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>{cool_string}</p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>I added this {goofy_func(5, 7)}</p>
+       <Student />
       </header>
       <footer>
         <p>Devin's Cool Geocities Site</p>
---
+++ src/components/Student.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Student = () => {
+  return (
+    <h3>Student Component</h3>
+  )
+}
+
+export default Student;
